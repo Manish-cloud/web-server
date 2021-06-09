dee99be64bc095ca09364f2bba3737717c051251
Manish-cloud <manishcloud9292@gmail.com>
2021-06-09 12:19:41 +0530

    Serving up HTML and JSON

--- src/app.js
+++ src/app.js
@@ -3,19 +3,30 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res) => {
-    res.send('Hello express !')
+    res.send('<h1>Weather</h1>')
 })
 
 app.get('/help', (req, res) => {
-    res.send('Help page')
+    res.send([
+        {
+            name: 'Manish',
+            age: 21
+        },
+        {
+            name : 'Shravan'
+        }
+    ])
 })
 
 app.get('/about', (req, res) => {
-    res.send('About page')
+    res.send('<h1>About page</h1>')
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Your weather')
+    res.send({
+        forecast:'Raining',
+        location: 'Pune'
+    })
 })
 
 app.listen(3000, () => {
