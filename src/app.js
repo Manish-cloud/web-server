607e5d8bcd36fb56232c6a89df40fc33d22e066d
Manish-cloud <manishcloud9292@gmail.com>
2021-06-10 23:11:02 +0530

    Integerating frontend with backend

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
 
@@ -39,9 +41,44 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
+    if(! req.query.address){
+        return res.send({
+            error: 'You must provide an address'
+        })
+    }
+    /*
     res.send({
         forecast:'Raining',
-        location: 'Pune'
+        location: 'Pune',
+        address: req.query.address
+    })
+    */
+    const address = req.query.address
+    geocode(address, (error, {latitude, longitude, location} = {}) => {
+        if (error) return res.send({ error })
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) return res.send({ error })
+
+            return res.send({
+                forecast: forecastData,
+                location,
+                address
+            })
+        })
+    })
+})
+
+app.get('/products', (req, res) => {
+    if(! req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query.search)
+    res.send({
+        products: []
     })
 })
 
